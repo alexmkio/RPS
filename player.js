7f8bd72d8a59e146e4b12d90e44ac6113ef01c1d
Alex Kio <alexandermartelkio@gmail.com>
2021-04-23 17:14:43 -0400

    Create localStorage functionality

--- player.js
+++ player.js
@@ -3,12 +3,16 @@ class Player {
     this.name = thisPlayer.name;
     this.emoji = thisPlayer.emoji;
     this.wins = 0;
+    this.choice;
   }
   saveWinsToStorage() {
-    
+    localStorage.setItem(this.name, JSON.stringify(this.wins))
   }
   retrieveWinsFromStorage() {
-    
+    var parsed = JSON.parse(localStorage.getItem(this.name));
+    if (parsed) {
+      this.wins = parsed;
+    }
   }
   takeTurn() {
     
