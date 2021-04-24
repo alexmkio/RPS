ddaa0173b44c9e9e2359c2fbd370aaff7d089d7a
Alex Kio <alexandermartelkio@gmail.com>
2021-04-24 18:28:50 -0400

    Create this.winner key and invoke function in main.js to update DOM

--- game.js
+++ game.js
@@ -3,12 +3,11 @@ class Game {
     this.players = [];
     this.gameType;
     this.whosTurn;
+    this.winner;
   }
   whoWon() {
-    console.log(this.players[0].name, this.players[0].choice)
-    console.log(this.players[1].name, this.players[1].choice)
     if (this.players[0].choice === this.players[1].choice) {
-      console.log(`DRAW`)
+      this.winner = 'draw';
     } else if (this.players[0].choice === 'Rock' && this.players[1].choice === 'Scissors' ||
     this.players[0].choice === 'Rock' && this.players[1].choice === 'Lizard' ||
     this.players[0].choice === 'Paper' && this.players[1].choice === 'Rock' ||
@@ -21,19 +20,19 @@ class Game {
     this.players[0].choice === 'Alien' && this.players[1].choice === 'Rock'
     ) {
       this.players[0].wins++
-      console.log(`${this.players[0].emoji} ${this.players[0].name} WON! ${this.players[0].emoji}`)
+      this.winner = this.players[0].name
+      // console.log(`${this.players[0].emoji} ${this.players[0].name} WON! ${this.players[0].emoji}`)
     } else {
       this.players[1].wins++
-      console.log(`${this.players[1].emoji} ${this.players[1].name} WON! ${this.players[1].emoji}`)
+      this.winner = this.players[1].name
+      // console.log(`${this.players[1].emoji} ${this.players[1].name} WON! ${this.players[1].emoji}`)
     }
     this.players[0].takeTurn()
     this.players[0].saveWinsToStorage()
     this.players[1].saveWinsToStorage()
-    console.log(this.players[0].wins)
-    console.log(this.players[1].wins)
-    console.log(this.whosTurn)
+    showWinner()
   }
-  resetBoard() {
-
+  resetBoard(e) {
+    handleChangeGameClick(e)
   }
 }
